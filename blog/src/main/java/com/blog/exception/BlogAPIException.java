6dfc53ef0ddbd88172cb75c37286f63cf1cbcf04
GirishHaragon <girishharagon115@gmail.com>
2023-06-18 09:28:45 +0530

    Done!

--- blog/src/main/java/com/blog/exception/BlogAPIException.java
+++
@@ -1,9 +0,0 @@
-package com.blog.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class BlogAPIException extends RuntimeException {
-    public BlogAPIException(String message) {
-        super(message);
-    }
-}
--- blog/src/main/java/com/blog/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.repositories;
-
-import com.blog.entities.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {//Instead of Ctrl+1 to import, in IntelliJ we use Alt+Enter
-}
